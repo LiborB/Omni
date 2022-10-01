5d5be6de7e74f20346d6b1ff241f83536e0a4156
Libor Bicanovsky <libor.bicanovsky@gmail.com>
2022-10-01 20:27:02 +1300

    Sync

--- omni-server/api/src/app.module.ts
+++ omni-server/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    type: "postgres",
+    port: 5432,
+    database: "omni",
+    entities: []
+  })],
   controllers: [AppController],
   providers: [],
 })
